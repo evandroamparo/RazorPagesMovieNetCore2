83009cfcf43df52c284fe92d891e68ee1241ade7
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2018-02-02 17:37:18 -0200

    Seed data

--- RazorPagesMovieNetCore2/Program.cs
+++ RazorPagesMovieNetCore2/Program.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RazorPagesMovieNetCore2.Models;
+using System;
 
 namespace RazorPagesMovieNetCore2
 {
@@ -7,7 +12,28 @@ namespace RazorPagesMovieNetCore2
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<MovieContext>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>

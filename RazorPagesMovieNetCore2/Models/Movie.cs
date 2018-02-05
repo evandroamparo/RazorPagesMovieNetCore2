a57be138a49837e241f617353cb441bca80bd92e
Evandro da Silva Amparo <evandro.amparo@gmail.com>
2018-02-05 09:30:43 -0200

    Add migration

--- RazorPagesMovieNetCore2/Models/Movie.cs
+++ RazorPagesMovieNetCore2/Models/Movie.cs
@@ -11,8 +11,8 @@ namespace RazorPagesMovieNetCore2.Models
         [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
